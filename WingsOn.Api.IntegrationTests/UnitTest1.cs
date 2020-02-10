3ad39d6519671585f95d7af896c4864820d18cf3
Safak Ulusoy <safakulusoy@gmail.com>
2020-02-11 00:02:39 +0100

    Introduced integration testing project for Api.

--- WingsOn.Api.IntegrationTests/UnitTest1.cs
+++ WingsOn.Api.IntegrationTests/UnitTest1.cs
@@ -1,4 +1,13 @@
 using System;
+using System.Runtime.Serialization.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using WingsOn.Application.Airlines.Resources;
 using Xunit;
 
 namespace WingsOn.Api.IntegrationTests
@@ -6,8 +15,32 @@ namespace WingsOn.Api.IntegrationTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async Task Test1()
         {
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHost =>
+                {
+                    // Add TestServer
+                    webHost.UseTestServer();
+                    webHost.UseStartup<Startup>();
+                    // webHost.Configure(app => app.Run(async ctx =>
+                    //    await ctx.Response.WriteAsync("Hello World!")));
+                });
+
+            // Build and start the IHost
+            var host = await hostBuilder.StartAsync();
+
+            var client = host.GetTestClient();
+
+            var httpResponse = await client.GetAsync("/airlines");
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var airlinesJsonString = await httpResponse.Content.ReadAsStringAsync();
+            var airlineResources = JsonSerializer.Deserialize<AirlineResource[]>(airlinesJsonString, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
 
         }
     }
